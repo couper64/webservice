c932d9440ca5c77313935193e772b8d2f3b7ce11
Vladislav Li <couper64@gmail.com>
2025-03-30 16:20:27 +0100

    Renamed app to api for clarity.

--- api/main.py
+++ api/main.py
@@ -7,9 +7,9 @@ from pydantic import BaseModel
 from pathlib import Path
 
 from celery.result import AsyncResult
-from app.task.sleep_task import sleep_task
+from api.task.sleep_task import sleep_task
 
-app = FastAPI()
+api = FastAPI()
 
 
 class RootResponse(BaseModel):
@@ -19,7 +19,7 @@ class RootResponse(BaseModel):
     timezone: str
 
 
-@app.get("/", response_model=RootResponse)
+@api.get("/", response_model=RootResponse)
 async def root():
 
     # Get current time in UTC
@@ -34,20 +34,20 @@ async def root():
 
 # The keyword include_in_schema=False included in the decorator hides the path operation from the schema used to autogenerate API docs.
 # The reason is that favicon requests (/favicon.ico) are automatically sent by browsers, but they are not part of the API’s core functionality. Including it in the schema could clutter the auto-generated API documentation (e.g., Swagger UI), which should focus on actual API endpoints. By using include_in_schema=False, you keep the API docs clean while still serving the favicon when requested.
-@app.get('/favicon.ico', include_in_schema=False)
+@api.get('/favicon.ico', include_in_schema=False)
 async def favicon():
 
     # The / operator in Path objects is overloaded to work as a path concatenation operator in Python’s pathlib module.
     return FileResponse(Path(__file__).parent / "favicon.ico")
 
 
-@app.post("/task/sleep")
+@api.post("/task/sleep")
 async def run_task(duration : float):
     result : AsyncResult = sleep_task.delay(duration)
     return {"task_id": result.id}
 
 
-@app.get("/task/{task_id}")
+@api.get("/task/{task_id}")
 async def get_task_status(task_id : str):
     result : AsyncResult = AsyncResult(task_id)
     return {"status": result.status, "result": result.result}
