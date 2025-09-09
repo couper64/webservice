895b744646857d1b07ee1f239035412a478122fb
Vladislav Li <couper64@gmail.com>
2025-09-09 01:15:33 +0100

    Added MinIO endpoints.

--- api/main.py
+++ api/main.py
@@ -1,17 +1,33 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
 from pathlib import Path
+from minio import Minio
 
 from api.router import task
 
+
+MINIO_BUCKET_NAME : str = "webservice"
+
+
 api = FastAPI()
 
-
 api.include_router(task.router)
+
+# MinIO client.
+minio_client = Minio(
+    endpoint="minio:9000", # MinIO endpoint
+    access_key="ROOTNAME",
+    secret_key="CHANGEME123",
+    secure=False # set True if using https
+)
+
+# Ensure bucket exists
+if not minio_client.bucket_exists(MINIO_BUCKET_NAME):
+    minio_client.make_bucket(MINIO_BUCKET_NAME)
 
 
 class RootResponse(BaseModel):
@@ -41,3 +57,22 @@ async def favicon():
 
     # The / operator in Path objects is overloaded to work as a path concatenation operator in Python’s pathlib module.
     return FileResponse(Path(__file__).parent / "favicon.ico")
+
+@api.get("/generate_upload_url/")
+def generate_upload_url(filename: str):
+    # Expire in 1 hour.
+    url = minio_client.presigned_put_object(
+        MINIO_BUCKET_NAME,
+        filename,
+        expires=timedelta(hours=1)
+    )
+    return {"upload_url": url}
+
+@api.get("/generate_download_url/")
+def generate_download_url(filename: str):
+    url = minio_client.presigned_get_object(
+        MINIO_BUCKET_NAME,
+        filename,
+        expires=timedelta(hours=1)
+    )
+    return {"download_url": url}
